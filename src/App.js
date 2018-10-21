670e71e17c2d62d08ee9063296d982fdad0d7229
abhidp <abhi358@gmail.com>
2018-10-22 10:49:55 +1100

    text gen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Output from "./Components/output";
+import Select from './Components/Controls/select'
+import axios from 'axios'
+const proxyURL = 'https://cors.io/?'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      paras: 4,
+      html: true,
+      text: ''
+    }
+  }
+
+  componentWillMount() {
+    this.getSampleText()
+  }
+
+  getSampleText() {
+    axios.get(proxyURL + 'http://hipsterjesus.com/api/?paras=' + this.state.paras + '&html=' + this.state.html)
+      .then((response) => {
+        this.setState({ text: response.data.text }, function () {
+          console.log(this.state)
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  showHtml(x) {
+    this.setState({ html: x }, this.getSampleText)
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+        <h1>ReactJS Sample Text Generator</h1>
+        <hr />
+        <form className='form-inline'>
+          <div className='form-group'>
+            <label>Include HTML:</label>
+            <Select value={this.state.html} onChange={this.showHtml.bind(this)} />
+          </div>
+        </form>
+        <Output value={this.state.text} />
       </div>
     );
   }
