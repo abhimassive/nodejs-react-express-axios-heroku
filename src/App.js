243a680cf7fb216579d9f469c8b2456358f51591
abhidp <abhi358@gmail.com>
2018-10-22 14:51:17 +1100

    completed 1 Sprint

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Output from "./Components/output";
 import Select from './Components/Controls/select'
+import Text from './Components/Controls/text';
 import axios from 'axios'
 const proxyURL = 'https://cors.io/?'
 
@@ -9,8 +10,8 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      paras: 4,
-      html: true,
+      paras: 2,
+      type: 'plaintext',
       text: ''
     }
   }
@@ -20,10 +21,10 @@ class App extends Component {
   }
 
   getSampleText() {
-    axios.get(proxyURL + 'http://hipsterjesus.com/api/?paras=' + this.state.paras + '&html=' + this.state.html)
+    axios.get(proxyURL + 'https://loripsum.net/api/' + this.state.paras + '/' + this.state.type)
       .then((response) => {
-        this.setState({ text: response.data.text }, function () {
-          console.log(this.state)
+        this.setState({ text: response.data }, function () {
+          // console.log(response.data)
         })
       })
       .catch((err) => {
@@ -31,8 +32,12 @@ class App extends Component {
       })
   }
 
-  showHtml(x) {
-    this.setState({ html: x }, this.getSampleText)
+  changeParas(number) {
+    this.setState({ paras: number }, this.getSampleText);
+  }
+
+  showType(x) {
+    this.setState({ type: x }, this.getSampleText)
   }
 
   render() {
@@ -40,13 +45,22 @@ class App extends Component {
       <div className="App container">
         <h1>ReactJS Sample Text Generator</h1>
         <hr />
+        <hr />
         <form className='form-inline'>
+          <div className="form-group">
+            <label>Paragraphs</label>
+            <Text value={this.state.paras} onChange={this.changeParas.bind(this)} />
+          </div>
           <div className='form-group'>
-            <label>Include HTML:</label>
-            <Select value={this.state.html} onChange={this.showHtml.bind(this)} />
+            <label>Format</label>
+            <Select value={this.state.type} onChange={this.showType.bind(this)} />
           </div>
         </form>
+        <br></br>
+
         <Output value={this.state.text} />
+
+
       </div>
     );
   }
